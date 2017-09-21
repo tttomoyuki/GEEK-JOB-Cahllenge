1a8dff0fad79cd1a401f7de09a58073161432732
EXP-0012\guest1Day <takedat3@gmai.com>
2017-09-21 21:22:20 +0900

    サーブレット課題　提出します

---
+++ CampChallenge_java/009.サーブレット/TestServlet.java
@@ -0,0 +1,100 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package org.mypackage.sample;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.util.Random;
+//ランダムで運勢を抽選するためのインポート
+/**
+ *
+ * @author guest1Day
+ */
+public class TestServlet extends HttpServlet {
+
+    /**
+     * Processes requests for both HTTP <code>GET</code> and <code>POST</code>
+     * methods.
+     *
+     * @param request servlet request
+     * @param response servlet response
+     * @throws ServletException if a servlet-specific error occurs
+     * @throws IOException if an I/O error occurs
+     */
+    protected void processRequest(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+        response.setContentType("text/html;charset=UTF-8");
+        try (PrintWriter out = response.getWriter()) {
+            
+          
+            String[] luckList = {"大吉", "中吉", "吉", "半吉", "末小吉", "凶", "小凶", "半凶", "末凶", "凶", "大凶"};
+            
+            Random rand = new Random(); //ランダムに数字を選ぶ準備
+            
+            Integer index = rand.nextInt(luckList.length);
+            //LuckListの中に入っている要素の数を数えて[index]に何番目か選んだものを渡している
+            
+          
+            out.print("あなたの運勢は・・・？" + luckList[index]);
+            
+            final String result ="/WEB-INF/jsp/rgn jsp.jsp";
+            
+           
+            
+            ReqestData data = new ResultData();
+            data.set D(new Data());
+            data.setLuck(luckList[]);
+            request.setAttribute("Data,data");
+           
+             RequestDispatcher nd = request.getDispatcher(result);
+            .forward(request, response);
+        }
+    }
+
+    // <editor-fold defaultstate="collapsed" desc="HttpServlet methods. Click on the + sign on the left to edit the code.">
+    /**
+     * Handles the HTTP <code>GET</code> method.
+     *
+     * @param request servlet request
+     * @param response servlet response
+     * @throws ServletException if a servlet-specific error occurs
+     * @throws IOException if an I/O error occurs
+     */
+    @Override
+    protected void doGet(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+        processRequest(request, response);
+    }
+
+    /**
+     * Handles the HTTP <code>POST</code> method.
+     *
+     * @param request servlet request
+     * @param response servlet response
+     * @throws ServletException if a servlet-specific error occurs
+     * @throws IOException if an I/O error occurs
+     */
+    @Override
+    protected void doPost(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+        processRequest(request, response);
+    }
+
+    /**
+     * Returns a short description of the servlet.
+     *
+     * @return a String containing servlet description
+     */
+    @Override
+    public String getServletInfo() {
+        return "Short description";
+    }// </editor-fold>
+
+}
