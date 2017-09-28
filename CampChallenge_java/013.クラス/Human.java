b39fc99d9b94f3f6e61c4c34bed87d14aaefc587
EXP-0012\guest1Day <takedat3@gmai.com>
2017-09-28 15:56:45 +0900

    クラスの利用、継承　課題提出します。

---
+++ CampChallenge_java/013.クラス/Human.java
@@ -0,0 +1,58 @@
+
+import static java.lang.System.out;
+
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ *
+ * @author guest1Day
+ */
+public class Human {
+
+
+    public String name ="java";
+    public int age = 0;
+   
+public void setHuman(String n,int a){
+        
+      name = n;
+      age = a;
+      
+      System.out.println(n);
+      System.out.println(a);
+      
+}
+
+
+
+    public static void main(String[] args) {
+        // Humanクラスのインスタンス(hito)生成
+        Human hito = new Human();
+
+        // 人インスタンスのageフィールドへ年齢設定
+        hito.age = 40;
+
+        // 人インスタンスのsetHumanメソッドを利用
+        //setHumanメソッドに"添田亮司"と38という値を設定し
+        //またsetHumanメソッドにはsoutが設定されているため呼び出すとそれらを表示する
+        hito.setHuman("添田亮司",38);
+        
+        
+        
+        
+    }
+}
+
+
+
+
+
+
+
+
+        
+
---
+++ CampChallenge_java/013.クラス/delete.java
@@ -0,0 +1,27 @@
+
+class  delete extends Human {
+    public void clear(){
+
+        this.name=null;
+        this.age=0;
+        
+        System.out.println(name+age);
+        
+        }
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+  
